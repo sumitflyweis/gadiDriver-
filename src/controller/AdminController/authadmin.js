3626b2c71254ffff9b13d9ed2f54e38536352186
varun chaudharyil.com <vcjagal1994@gmail.com>
2023-07-02 09:40:27 +0530

    update

--- src/controller/AdminController/authadmin.js
+++ src/controller/AdminController/authadmin.js
@@ -1,4 +1,4 @@
-const User = require("../../model/userModel");
+const User = require("../../model/authadmin");
 const bcrypt = require("bcryptjs")
 const jwt = require("jsonwebtoken")
 const nodemailer = require('nodemailer');
