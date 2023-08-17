33ea9ef1e6a672ef31865aaafe52d8f4f8d90f0b
rubiyadav <rubiflyeis@gmail.com>
2023-08-17 15:31:40 +0530

    create an api

--- src/controller/EmployeerController/employee.js
+++ src/controller/EmployeerController/employee.js
@@ -145,7 +145,7 @@ exports.salarayAndExpectation = async (req, res) => {
 };
 exports.updateJobServices = async (req, res) => {
   try {
-    const jobService = await JobService.findById({ _id: req.params._id });
+    const jobService = await JobService.findById({ _id: req.params.id });
     if (!jobService) {
       return res.status(404).json({ status: 404, message: "Job service not found" });
     } else {
